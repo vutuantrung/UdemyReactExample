b16a3a891f535df95c234582148ec583580165b6
VUTuanTrung <trung.lespace@gmail.com>
2021-01-03 12:44:41 +0100

    working with price and button

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 function Burger(props) {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />;
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredient</p>
+    }
 
     return (
         <div className={classes.Burger}>
