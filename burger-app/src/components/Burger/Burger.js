a03afc4ea36cf73983e9b5db97ae7e68925b05e8
VUTuanTrung <trung.lespace@gmail.com>
2021-01-03 11:13:50 +0100

    implement components + css and add module js prop-types

---
+++ burger-app/src/components/Burger/Burger.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Burger.module.css';
+
+function Burger(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Burger;
--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import classes from './Layout.module.css';
 import Auxiliary from '../../hoc/Auxiliary';
 
 function Layout(props) {
     return (
         <Auxiliary>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={classes.content}>
                 {props.children}
             </main>
         </Auxiliary>
