66ac7ef05a22c8d4c5f103b4c2021bfe6f3d7d41
VUTuanTrung <trung.lespace@gmail.com>
2021-01-04 14:58:01 +0100

    Adding Spinner css

---
+++ burger-app/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Spinner.module.css';
+
+function Spinner() {
+    return (
+        <div class={classes.Loader}>Loading...</div>
+    )
+}
+
+export default Spinner
