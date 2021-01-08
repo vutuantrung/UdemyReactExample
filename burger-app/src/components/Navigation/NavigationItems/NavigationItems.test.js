c1e3f8407504d7cbd07e11928a79792dade24289
VUTuanTrung <trung.lespace@gmail.com>
2021-01-08 16:07:23 +0100

    add first unit test

---
+++ burger-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,13 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItem from './NavigationItem/NavigationItem';
+import NavigationItems from './NavigationItems';
+
+configure({ adapter: new Adapter() });
+
+describe('<NavigationItems />', () => {
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+})
